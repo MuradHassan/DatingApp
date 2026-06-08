d5df465a366827f3ae09541ade4be4dcab624c3c
Murad Hassan <muraad.180@gmail.com>
2026-06-08 22:47:53 +0300

    Initial Create

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MembersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await _context.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await _context.Users.FindAsync(id);
+            if (member == null) return NotFound();
+            return member;
+        }
+    }
+}
---
+++ API/Entites/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entites
+{
+    public class AppUser
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Displayname { get; set; }
+        public required string Email { get; set; }
+    }
+}
