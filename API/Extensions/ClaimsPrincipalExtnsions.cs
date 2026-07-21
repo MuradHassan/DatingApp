c5d3a0ba791c2dd31009a8b79355c7100cc12560
Murad Hassan <muraad.180@gmail.com>
2026-07-22 01:35:09 +0300

    Updating resources

---
+++ API/Extensions/ClaimsPrincipalExtnsions.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipalExtnsions
+    {
+
+        public static string GetMemberId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? throw new Exception("Cannot get memberId from token");
+        }
+    }
+}
--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,21 +1,72 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../../types/member';
+import {
+  Component,
+  HostListener,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
+import { MemberService } from '../../../core/services/member-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../../core/services/toast-service';
+import { AccountService } from '../../../core/services/account-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css',
 })
-export class MemberProfile implements OnInit {
-  private route = inject(ActivatedRoute);
-  protected member = signal<Member | undefined>(undefined);
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notity($event: BeforeUnloadEvent) {
+    if (this.editForm?.dirty) {
+      $event.preventDefault();
+    }
+  }
+  private accountSrvice = inject(AccountService);
+  protected memberService = inject(MemberService);
+  private toast = inject(ToastService);
+  protected editbleMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: '',
+  };
 
   ngOnInit(): void {
-    this.route.parent?.data.subscribe((data) => {
-      this.member.set(data['member']);
+    this.editbleMember = {
+      displayName: this.memberService.member()?.displayName || '',
+      description: this.memberService.member()?.description || '',
+      city: this.memberService.member()?.city || '',
+      country: this.memberService.member()?.country || '',
+    };
+  }
+
+  ngOnDestroy(): void {
+    if (this.memberService.editMode()) {
+      this.memberService.editMode.set(false);
+    }
+  }
+
+  updateProfile() {
+    if (!this.memberService.member()) return;
+    const updateMember = { ...this.memberService.member(), ...this.editbleMember };
+    this.memberService.updateMember(this.editbleMember).subscribe({
+      next: () => {
+        const currentUsr = this.accountSrvice.currentUser();
+        if (currentUsr && updateMember.displayName !== currentUsr?.displayName) {
+          currentUsr.displayName = updateMember.displayName;
+          this.accountSrvice.setCurrentUser(currentUsr);
+        }
+        this.toast.success('Profile updated successfully');
+        this.memberService.editMode.set(false);
+        this.memberService.member.set(updateMember as Member);
+        this.editForm?.reset(updateMember);
+      },
     });
   }
 }
