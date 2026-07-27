26d7024d03f74b31e2bf410cf377c7083718e49a
Murad Hassan <muraad.180@gmail.com>
2026-07-27 03:10:49 +0300

    Reactive forms

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,13 +6,19 @@ namespace API.DTOs
     {
         [Required]
         public string DisplayName { get; set; } = string.Empty;
-
         [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
-
         [Required]
         [MinLength(4)]
         public string Password { get; set; } = string.Empty;
+        [Required]
+        public string Gender { get; set; } = string.Empty;
+        [Required]
+        public string City { get; set; } = string.Empty;
+        [Required]
+        public string Country { get; set; } = string.Empty;
+        [Required]
+        public DateOnly DateOfBirth { get; set; }
     }
 }
