fbf7176f9bcdc6989203144995496b507e35af74
Murad Hassan <muraad.180@gmail.com>
2026-08-16 18:00:39 +0300

    Adding a Likes feature

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,67 @@
+﻿using API.Entites;
+using API.Helpers;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class LikesRepository(AppDbContext context) : ILikesRepository
+    {
+        public void AddLike(MemberLike like)
+        {
+            context.Likes.Add(like);
+        }
+
+        public void DeleteLike(MemberLike like)
+        {
+            context.Likes.Remove(like);
+        }
+
+        public async Task<IReadOnlyList<string>> GetCurrentMemberLikeIds(string memberId)
+        {
+            return await context.Likes
+                .Where(x => x.SourceMemberId == memberId)
+                .Select(x => x.TargetMemberId)
+                .ToListAsync();
+        }
+
+        public async Task<PaginatedResult<Member>> GetMemberLikes(LikesParams likesParams)
+        {
+            var query = context.Likes.AsQueryable();
+            IQueryable<Member> result;
+
+            switch (likesParams.Predicate)
+            {
+                case "liked":
+                    result = query
+                        .Where(like => like.SourceMemberId == likesParams.MemberId)
+                        .Select(like => like.TargetMember);
+                    break;
+                case "likedBy":
+                    result = query
+                        .Where(like => like.TargetMemberId == likesParams.MemberId)
+                        .Select(like => like.SourceMember);
+                    break;
+                default: //mutual
+                    var likedIds = await GetCurrentMemberLikeIds(likesParams.MemberId);
+                    result = query
+                        .Where(x => x.TargetMemberId == likesParams.MemberId
+                        && likedIds.Contains(x.SourceMemberId))
+                        .Select(x => x.SourceMember);
+                    break;
+            }
+
+            return await PaginationHelper.CreateAsync(result, likesParams.PageNumber, likesParams.PageSize);
+        }
+
+        public async Task<MemberLike?> GetMemberLike(string sourceMemberId, string likedMemberId)
+        {
+            return await context.Likes.FindAsync(sourceMemberId, likedMemberId);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Entites;
+using API.Helpers;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<MemberLike?> GetMemberLike(string sourceMemberId, string likedMemberId);
+        Task<PaginatedResult<Member>> GetMemberLikes(LikesParams likesParams);
+        Task<IReadOnlyList<string>> GetCurrentMemberLikeIds(string memberId);
+        void DeleteLike(MemberLike like);
+        void AddLike(MemberLike like);
+        Task<bool> SaveAllAsync();
+    }
+}
